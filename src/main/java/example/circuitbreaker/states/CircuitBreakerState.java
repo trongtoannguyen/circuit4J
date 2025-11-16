f93151696048adb364cf8ad188a0baab18385c3d
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-11-16 15:48:15 +0700

    add methods for state transitions and invocation in CircuitBreakerState interface

--- src/main/java/example/circuitbreaker/states/CircuitBreakerState.java
+++ src/main/java/example/circuitbreaker/states/CircuitBreakerState.java
@@ -1,4 +1,19 @@
 package example.circuitbreaker.states;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
 public interface CircuitBreakerState {
+
+    void enter();
+
+    void invocationFails();
+
+    void invocationSucceeds();
+
+    void invoke(Runnable action);
+
+    <T> T invoke(Supplier<T> func);
+
+    <T> CompletableFuture<T> invokeAsync(Supplier<CompletableFuture<T>> func);
 }
